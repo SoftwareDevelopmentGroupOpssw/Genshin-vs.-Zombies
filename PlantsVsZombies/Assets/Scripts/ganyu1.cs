e311f286175977d64a5e0e7ef677dcffd326b3e9
qwerdfzzs <1507493974@qq.com>
2022-12-21 12:51:25 +0800

    problems to solve

---
+++ PlantsVsZombies/Assets/Scripts/ganyu1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ganyu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  GameObject.Instantiate(arrow);
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
